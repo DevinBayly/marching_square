8266928c61ed6c5157438dc15e4ff94f23fc58c2
Devin Bayly <>
2021-05-15 18:29:14 +0000

    created_loader

--- script.js
+++ script.js
@@ -1,166 +1,109 @@
-let data
-let canRun = false
-let pt = (x, y) => ({
-  x, y,
-  draw() {
-    point(this.x, this.y)
+
+let pt = (x, y, z) => ({
+  x, y, z,
+  mult(v) {
+    let res = { ...this }
+    res.x *= v
+    res.y *= v
+    res.z *= v
+    return res
   },
-  dist(opt) {
-    return ((this.x- opt.x)**2+(this.y - opt.y)**2)**.5
+  dot(o) {
+    return this.x * o.x + this.y * o.y + this.z * o.z
+  },
+  sub(o) {
+    let o_neg = o.mult(-1)
+    return this.add(o_neg)
+  },
+  add(o) {
+    let res = { ...this }
+    res.x += o.x
+    res.y += o.y
+    res.z += o.z
+    return res
+  },
+  magnitude() {
+    return Math.sqrt(this.x * this.x + this.y * this.y + this.z * this.z)
   }
 })
 
-let sq = (center, sqw, sqh) => ({
-  top: center.y + sqh / 2,
-  bottom: center.y - sqh / 2,
-  left: center.x - sqw / 2,
-  right: center.x + sqw / 2,
-  center,
-  sqw,
-  sqh,
-  draw() {
-    rect(this.left, this.bottom, this.sqw, this.sqh)
-  },
-  contains(p) {
-    //console.log("checking contains", p, p.x >= this.left, p.x <= this.right, p.y <= this.top, p.y >= this.bottom)
-    return p.x >= this.left && p.x <= this.right && p.y <= this.top && p.y >= this.bottom
-  }
-})
-// //
-let QT = (center, qtw, qth, lim) => ({
-  square: sq(center, qtw, qth),
-  qtw,
-  qth,
-  subdivided: false,
-  points: [],
-  subqt: [],
-  center,
-  lim,
-  add(pele) {
-    //console.log("adding", pele)
-    // check whether the point is within our sq
-    if (this.subdivided) {
-      // add it to one of the children
-      for (let subqt of this.subqt) {
-        subqt.add(pele)
-      }
-    } else if (!this.square.contains(pele)) { // if the point isn't inside our square, then skip
-      return
-    } else if (this.points.length < this.lim - 1) {
-      // add the point to the list
-      //console.log("adding point")
-      this.points.push(pele)
-    } else {
-      // subdivided
-      // make subtrees
-      // top left
-      let tl = QT(pt(this.center.x - this.qtw / 4, this.center.y - this.qth / 4), this.qtw / 2, this.qth / 2, this.lim)
-      let tr = QT(pt(this.center.x + this.qtw / 4, this.center.y - this.qth / 4), this.qtw / 2, this.qth / 2, this.lim)
-      let bl = QT(pt(this.center.x - this.qtw / 4, this.center.y + this.qth / 4), this.qtw / 2, this.qth / 2, this.lim)
-      let br = QT(pt(this.center.x + this.qtw / 4, this.center.y + this.qth / 4), this.qtw / 2, this.qth / 2, this.lim)
-      this.subqt = [tl, tr, bl, br]
-      // for each point
-      // also add our current pele to someone
+let clicks = 0
+let a, b, c
 
-      for (let p of [...this.points, pele]) {
-        for (let subqt of this.subqt) {
-          subqt.add(p)// will add it if it makes sense
-        }
-      }
-      // change subdivided
-      this.subdivided = true
-      this.points = []
-    }
-  },
-  draw() {
-    //console.log(this)
-    strokeWeight(1)
-    this.square.draw()
-    // if subdivided call draw on our children
-    if (this.subdivided) {
-      //console.log("is subdivided")
-      for (let subqt of this.subqt) {
-        subqt.draw()
-      }
-    } else {
-      // draw each point
-      //console.log("not subdivided")
-      strokeWeight(4)
-      for (let p of this.points) {
-        p.draw()
-      }
-    }
-
-
-    // want to draw our boxes and the points 
-
-  },
-  query(pele) {
-    let result = []
-    if (!this.square.contains(pele)) {
-      return []
-    } else {
-      if (this.subdivided) {
-        for (let subqt of this.subqt) {
-          result = [...result,...subqt.query(pele)]
-        }
-      } else {
-        result = this.points
-      }
-    }
-    return result
-  }
-  // has subdivided bool
-  // has subs which are also QTs
-  // points if not subdivided
-  // method add
-  // method query
-  // width, height
-
-})
-
-function preload() {
-
-  fetch("test_vtu_convert_tstep10.json").then(res => res.json()).then(j => {
-    data = j
-    canRun = true
-    setup()
-  })
-}
-let qt
+let faces = []
+let points = []
 function setup() {
-  if (!canRun) {
-    return
-  }
-  console.log("setup")
-  createCanvas(400, 400)
-  background("white")
-  noFill()
-  strokeWeight(1)
-  qt = QT(pt(width / 2, height / 2), width, height, 100)
-  // make random data
-  // Array(40).fill(0).map(e=> qt.add(pt(random(width),random(height))))
-  // use the points that are in the data
-  // 
-  let scalex = d3.scaleLinear().range([0,width])
-  let scaley = d3.scaleLinear().range([height,0])
-  // 
-  let localdata = data.points.slice(40)
-  scalex.domain(d3.extent(localdata.map(e=> e.x)))
-  scaley.domain(d3.extent(localdata.map(e=> e.y)))
-  localdata.map(e=> {
-    qt.add(pt(scalex(e.x),scaley(e.y)))
+  createCanvas(500, 500)
+  let loader = new THREE.GLTFLoader()
+  loader.load("./demo_mesh.glb", function (model) {
+    //console.log(model)
+    let geo = model.scene.children[0].geometry
+    let values = geo.attributes.position.array
+    //console.log(values)
+    let indices = geo.index.array
+    for (let i = 0; i < values.length; i += 3) {
+      let p = new THREE.Vector3(values[i] * width, height - values[i + 1] * height, values[i + 2])
+      points.push(p)
+    }
+    //console.log("points", points)
+    // in theory we have 64 faces, and 81 vertices
+    for (let i = 0; i < indices.length; i += 3) {
+      // use the values in the first 4 to create a face
+      let face = []
+      let faceIndices = indices.slice(i, i + 3)
+      for (let ind of faceIndices) {
+        face.push(points[ind])
+      }
+      faces.push(face)
+    }
+    strokeWeight(10)
+    drawMesh()
+
   })
-  qt.draw()
-
-
-
-  // stroke("black")
-  // line(test.x - 50,test.y,test.x + 50,test.y)
 }
+
+function drawMesh() {
+  stroke("black")
+  for(let f of faces) {
+    for (let i = 0; i < 3; i += 1) {
+      let p1 = f[i % 3]
+      let p2 = f[(i + 1) % 3]
+      line(p1.x, p1.y, p2.x, p2.y)
+      //point(p1.x*width,height - p1.y*height)
+    }
+  }
+}
+
+function barycentricContains(a, b, c, p) {
+  // 
+  let v1 = b.sub(a)
+  let v2 = c.sub(a)
+  let v3 = p.sub(a)
+
+  let v =(v3.y*v1.x - v3.x*v1.y)/(v2.y*v1.x - v2.x*v1.y)
+  let u = v3.x/v1.x -v*v2.x/v1.x
+  let uv = v+u
+  return uv >= 0 && uv <= 1
+}
+
 function mousePressed() {
-  console.log("clicked")
-  let neighbors = qt.query(pt(mouseX,mouseY))
-  stroke('red')
-  neighbors.map(e=> point(e.x,e.y))
+  // 
+  drawMesh()
+  for (let f of faces) {
+  let p = new THREE.Vector3(mouseX, mouseY, 0)
+    let p1 = f[0]
+    let p2 = f[1]
+    let p3 = f[2]
+    stroke("black")
+    // 
+    let tri = new THREE.Triangle(p1,p2,p3)
+
+    if (tri.containsPoint(p)) {
+      stroke("red")
+      point(p1.x, p1.y)
+      point(p2.x, p2.y)
+      point(p3.x, p3.y)
+    }
+  }
+
 }
